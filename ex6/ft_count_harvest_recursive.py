72aaa5d723c4d8efd89a1365b27812c39337b0f5
anas <anaslibrini@gmail.com>
2026-09-23 20:46:06 +0100

    done

--- ex6/ft_count_harvest_recursive.py
+++ ex6/ft_count_harvest_recursive.py
@@ -2,15 +2,10 @@
 def helper_count(i , number):
     if i == number + 1:
         return
-    print(i)
+    print("Days", i)
     helper_count(i + 1, number)
-     
-        
-    
 
 def ft_count_harvest_recursive():
     number = int(input("Days until harvest: "))
     helper_count(1, number)
-
-
-ft_count_harvest_recursive()
+    print("Harvest time!")
